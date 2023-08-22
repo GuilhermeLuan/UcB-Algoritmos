f09602318626d2641f9a80015c69e033fbcd7879
Guilherme Luan <guilhermeluan@pm.me>
2023-08-22 09:14:32 -0300

    feat: aula 3 e 4

---
+++ Aula 3 e 4/calculadora.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero1 = 33;
+    int numero2 = 2;
+    int resultado;
+
+    resultado = numero1 / numero2;
+    printf("Resultado: %d\n", resultado);
+
+}
