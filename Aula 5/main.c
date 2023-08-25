fc4a5845916e15e57ecf039ca4ea9d42e7aa6aac
Guilherme Luan <guilhermeluan@pm.me>
2023-08-25 10:29:59 -0300

    feat: aula 5

---
+++ Aula 5/main.c
@@ -0,0 +1,15 @@
+//Operadores relacional -> comparação de valores.
+    // > < >= <= == !=
+//Operadores logicos -> combinar condiçõe 
+    // && || ! 
+//Operadores logicos a nivel bit -> apenas um simbolo
+    // ~ -> Inverte os numeros binarios
+    // &
+    // |
+
+#include <stdio.h>
+
+int main(){
+    int numero1 = 8;
+    int numero2 = 10;
+}
